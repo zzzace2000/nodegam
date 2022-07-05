84cd7ed7391c848e8d960620ab27d999f92a87ee
Chun-Hao Chang <zzzace2000@gmail.com>
2022-07-05 14:42:16 -0400

    Add notebooks

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 name = "nodegam"
-version = "0.2.0"
+version = "0.2.1"
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
